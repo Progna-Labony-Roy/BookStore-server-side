049a7d38f9d2d2c54fc0da25e3a7b06ef8ba347f
Progna_Labony_Roy <labony.progna@gmail.com>
2022-11-25 07:55:05 +0600

    connected with database

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express =require('express');
 const cors=require('cors');
 const app=express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-//resaleServer
-//GH9GfzYN44zmAti1
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m9nbseb.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+  try{
+
+  }
+
+  finally{
+    
+  }
+}
+run().catch(concole.log)
+
+
 
 app.get('/',(req,res) =>{
     res.send('Resale server Running');
