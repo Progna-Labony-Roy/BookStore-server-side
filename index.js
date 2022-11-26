7176324135a66dd1992eab30370ac7a6b5c5f770
Progna_Labony_Roy <labony.progna@gmail.com>
2022-11-26 12:07:07 +0600

    category books api

--- index.js
+++ index.js
@@ -1,44 +1,70 @@
-const express =require('express');
-const cors=require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-const app=express();
+const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m9nbseb.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+async function run() {
+  try {
+    const categoriesCollection = client
+      .db("resaleDatabase")
+      .collection("categories");
 
-async function run(){
-  try{
-const categoriesCollection = client.db('resaleDatabase').collection('categories');
-  
-app.get('/categories',async(req,res) =>{
-    const query = {} ;
-    const options = await categoriesCollection.find(query).toArray();
-    res.send(options);
-})
-  }
+    const bookCollection = client.db("resaleDatabase").collection("books");
 
-  finally{
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const options = await categoriesCollection.find(query).toArray();
 
+      res.send(options);
+    });
+
+    // app.get("/categories/:id", async (req, res) => {
+    //   const id = req.query.id;
+    //   const category_books = books.filter((b) => b.category_id === id);
+    //   const query = { category_books };
+    //   const options = await categoriesCollection.find(query).toArray();
+
+    //   res.send(options);
+    // });
+
+    app.get("/books", async (req, res) => {
+      const query = {};
+      const books = await bookCollection.find(query).toArray();
+      res.send(books);
+    });
+
+    app.get("/books/:id", async (req, res) => {
+      const id = req.query.id;
+      const query = { category_name: id };
+      const books = await bookCollection.find(query).toArray();
+      res.send(books);
+    });
+
+    
+  } finally {
   }
 }
-run().catch(console.log)
+run().catch(console.log);
 
+app.get("/", (req, res) => {
+  res.send("Resale server Running");
+});
 
-
-app.get('/',(req,res) =>{
-    res.send('Resale server Running');
-})
-
-app.listen(port,()=>{
-    console.log(`Simple server running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Simple server running on port ${port}`);
+});
